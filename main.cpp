7b335ad1ce43edae5300c85e0f073dad6aaa31e5
Vera Alexeeva <vera.cortie@gmail.com>
2020-02-17 20:43:03 +0300

    polymorphysm

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+class A{
+public:
+	int x;
+	A() : x(1) {};
+	virtual void f(){
+		std::cout << "A()" << std::endl;
+	}
+};
+
+class B: public A {
+public:
+	int y;
+	B() : y(2) {};
+	virtual void f(){
+		std::cout << "B()" << std::endl;
+	}
+	void g() {
+		y = 3;
+		std::cout << "g" << std::endl;
+	};
+};
+
+int main(){
+	A a;
+	a.f();
+	A * ab = new B();
+	ab->f();
+	return 0;
+}
